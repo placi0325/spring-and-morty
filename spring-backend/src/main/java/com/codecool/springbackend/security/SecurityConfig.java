19a6f63e05e4765b68d0d682ba7f86ac8a1624c1
PeterfiLaci <113706063+placi0325@users.noreply.github.com>
2023-10-19 10:35:22 +0200

    add react build

--- spring-backend/src/main/java/com/codecool/springbackend/security/SecurityConfig.java
+++ spring-backend/src/main/java/com/codecool/springbackend/security/SecurityConfig.java
@@ -3,21 +3,33 @@ package com.codecool.springbackend.security;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests()
+                .addFilterBefore(jwtAuthenticationFilter, BasicAuthenticationFilter.class)
+                .authorizeHttpRequests((auth) -> {
+                    auth.anyRequest().permitAll();
+                    /*auth.requestMatchers("/").permitAll();
+                    auth.requestMatchers(HttpMethod.GET, "/").permitAll();
+                    auth.requestMatchers(HttpMethod.POST,"/").permitAll();
+                    auth.requestMatchers(HttpMethod.GET,"/").hasAnyAuthority(Role.USER.toString(), Role.ADMIN.toString());
+                    auth.anyRequest().authenticated();*/
+                }).sessionManagement((session) -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                );
 
 
         return http.build();
